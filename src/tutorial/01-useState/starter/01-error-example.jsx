105982498deea4e4551d72815dd817856be8fd80
RedJyu <piotr.stezycki@protonmai.com>
2023-08-29 14:32:16 +0200

    learning basic of usestate

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,19 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button
+        type='button'
+        onClick={() => {
+          count = count + 1;
+        }}
+      >
+        increase
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // const value = useState('hello')[0];
+  // const func = useState('hello')[1];
+
+  // console.log(value);
+  // console.log(func);
+
+  const [count, setCount] = useState(0);
+  const click = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h4>Clicked {count} times</h4>
+      <button type='button' className='btn' onClick={click}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;

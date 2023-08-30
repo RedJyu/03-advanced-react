ddbf699fac3ac4cd161f3eecba1a01ae9b548501
RedJyu <piotr.stezycki@protonmai.com>
2023-08-30 13:29:19 +0200

    use state on array

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const remove = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  const removeAll = () => {
+    setPeople([]);
+  };
+  return (
+    <>
+      <div>
+        {people.map((person) => {
+          const { id, name } = person;
+          return (
+            <div key={id}>
+              <h4>{name}</h4>
+              <button type='button' className='btn' onClick={() => remove(id)}>
+                remove
+              </button>
+            </div>
+          );
+        })}
+        <button type='button' className='btn' onClick={removeAll}>
+          remove all
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default UseStateArray;

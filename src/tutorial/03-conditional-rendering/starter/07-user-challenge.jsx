8ad4ef5a465139a5daded5673eef09de3b4716d7
RedJyu <piotr.stezycki@protonmai.com>
2023-09-07 10:53:38 +0200

    user challenge complete

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,28 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react';
 
+const UserChallenge = () => {
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({ name: 'John Test' });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there {user.name}</h4>
+          <button onClick={logout}>logout</button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please login</h4>
+          <button onClick={login}>login</button>
+        </div>
+      )}
+    </div>
+  );
+};
 export default UserChallenge;

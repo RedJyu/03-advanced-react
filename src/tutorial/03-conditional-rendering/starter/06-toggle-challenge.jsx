acb028b7e1927d0e354358017a876d2ffc9c2391
RedJyu <piotr.stezycki@protonmai.com>
2023-09-07 10:28:35 +0200

    toggle challenge complete

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <div>
+      <h2>{toggle ? 'hello' : 'good bye'}</h2>
+      <button
+        type='button'
+        className='btn'
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default ToggleChallenge;

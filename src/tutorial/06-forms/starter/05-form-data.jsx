f1a18f8a22c97a643a7af60710066681fb805b46
RedJyu <piotr.stezycki@protonmai.com>
2023-09-18 12:16:18 +0200

    form data api

--- src/tutorial/06-forms/starter/05-form-data.jsx
+++ src/tutorial/06-forms/starter/05-form-data.jsx
@@ -5,6 +5,13 @@ const UncontrolledInputs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // const name = formData.get('name');
+    // console.log([...formData.entries()]);
+    const newUser = Object.fromEntries(formData);
+    console.log(newUser);
+    setValue(value + 1);
+    e.currentTarget.reset();
   };
   return (
     <div>
@@ -24,7 +31,7 @@ const UncontrolledInputs = () => {
           </label>
           <input type='email' className='form-input' id='email' name='email' />
         </div>
-        {/* email */}
+        {/* password */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password

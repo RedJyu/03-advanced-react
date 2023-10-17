5e25db52884b7e33b04307323fd5e6275593950c
RedJyu <piotr.stezycki@protonmai.com>
2023-10-17 13:23:08 +0200

    performance challenge finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import ReducerBasics from './tutorial/10-useReducer/starter/01-useReducer';
-import Index from './tutorial/11-performance/starter/01-lower-state';
+import Index from './tutorial/11-performance/starter/02-lower-state-challenge';
 
 function App() {
   return (

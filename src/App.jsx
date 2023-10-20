1eb8789f4b2be9dd5611e039dc95028a9ec74873
RedJyu <piotr.stezycki@protonmai.com>
2023-10-20 12:35:23 +0200

    using memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import ReducerBasics from './tutorial/10-useReducer/starter/01-useReducer';
-import Index from './tutorial/11-performance/starter/02-lower-state-challenge';
+import Index from './tutorial/11-performance/starter/03-hooks/';
 
 function App() {
   return (

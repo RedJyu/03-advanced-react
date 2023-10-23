831265fdefd98de86b6822499c0684e253aee734
RedJyu <piotr.stezycki@protonmai.com>
2023-10-23 12:29:07 +0200

    use transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import ReducerBasics from './tutorial/10-useReducer/starter/01-useReducer';
-import Index from './tutorial/11-performance/starter/03-hooks/';
+import Index from './tutorial/11-performance/starter/04-react-18/index';
 
 function App() {
   return (
